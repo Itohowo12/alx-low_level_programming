5a32cc96ddd4d2c3b6561e0a5683fff8db56825c
itohowo12 <tomitohowoime@gmail.com>
2022-12-17 20:48:35 +0100

    Print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,16 @@
 * print_numbers - print numbers
 * Return: Always 0.
 */
-void print_numbers (void)
+void print_numbers(void)
 {
 int i;
 
 i = 0;
 while (i < 10)
 {
-_putchar (i + '0')
+_putchar (i + '0');
 i++;
 }
+
 _putchar ('\n');
 }
