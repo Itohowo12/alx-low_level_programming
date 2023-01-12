d1a1f993c4da4eb72c2c6e37f658837ebf25f66d
itohowo12 <tomitohowoime@gmail.com>
2023-01-12 03:55:51 +0100

    my zero task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,30 +8,30 @@
  */
 char *create_array(unsigned int size, char c)
 {
-        char *buffer;
-        unsigned int position;
+	char *buffer;
+	unsigned int position;
 
-        if (size == 0)
-        {
-                return (NULL);
-        }
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-        /*Define values with malloc*/
-        buffer = (char *) malloc(size * sizeof(c));
+	/*Define values with malloc*/
+	buffer = (char *) malloc(size * sizeof(c));
 
-        if (buffer == 0)
-        {
-                return (NULL);
-        }
-        else
-        {
-                position = 0;
-                while (position < size) /*While for array*/
-                {
-                        *(buffer + position) = c;
-                        position++;
-                }
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+		while (position < size) /*While for array*/
+		{
+			*(buffer + position) = c;
+			position++;
+		}
 
-                return (buffer);
-        }
+		return (buffer);
+	}
 }
