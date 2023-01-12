1a034968a90660c7b735d340909a7f5b3853d741
itohowo12 <tomitohowoime@gmail.com>
2023-01-12 03:53:54 +0100

    my first task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,15 +7,15 @@
 
 int _strlen(char *s)
 {
-        unsigned int i;
+	unsigned int i;
 
-        i = 0;
-        while (s[i] != '\0') /*count character of string*/
-        {
-                i++;
-        }
+	i = 0;
+	while (s[i] != '\0') /*count character of string*/
+	{
+		i++;
+	}
 
-        return (i);
+	return (i);
 }
 
 /**
@@ -27,16 +27,16 @@ int _strlen(char *s)
 
 char *_strcpy(char *dest, char *src)
 {
-        int i = 0;
+	int i = 0;
 
-        while (src[i] != '\0')
-        {
-                dest[i] = src[i];
-                i++;
-        }
-        dest[i] = '\0';
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 
-        return (dest);
+	return (dest);
 }
 
 /**
@@ -47,23 +47,23 @@ char *_strcpy(char *dest, char *src)
 
 char *_strdup(char *str)
 {
-        char *dst;
-        unsigned int size;
+	char *dst;
+	unsigned int size;
 
-        if (str == 0)
-        {
-                return (NULL);
-        }
+	if (str == 0)
+	{
+		return (NULL);
+	}
 
-        size = _strlen(str) + 1;
+	size = _strlen(str) + 1;
 
-        dst = (char *) malloc(size * sizeof(char));
+	dst = (char *) malloc(size * sizeof(char));
 
-        if (dst == 0)
-        {
-                return (NULL);
-        }
+	if (dst == 0)
+	{
+		return (NULL);
+	}
 
-        _strcpy(dst, str);
-        return (dst);
+	_strcpy(dst, str);
+	return (dst);
 }
